2fbcc120b0841a7f977d716b6bd6aeb4741104f2
Graziele Fernandes de Oliveira <20211204010046@ifrn.local>
2024-11-14 14:07:40 -0300

    criando Repository para o projeto

--- src/main/java/ifrn/pf/projeto/controllers/ProjetoController.java
+++ src/main/java/ifrn/pf/projeto/controllers/ProjetoController.java
@@ -1,12 +1,17 @@
 package ifrn.pf.projeto.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import ifrn.pf.projeto.models.Projeto;
+import ifrn.pf.projeto.repositories.ProjetoRepository;
 
 @Controller
 public class ProjetoController {
+	
+	@Autowired
+	private ProjetoRepository pr;
 	
 	@RequestMapping("/projeto/form")
 	public String form() {
@@ -17,6 +22,7 @@ public class ProjetoController {
 	public String adicionar(Projeto projeto) {
 		
 		System.out.println(projeto);
+		pr.save(projeto);
 		
 		return "projeto-adicionado";
 	}
---
+++ src/main/java/ifrn/pf/projeto/repositories/ProjetoRepository.java
@@ -0,0 +1,9 @@
+package ifrn.pf.projeto.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import ifrn.pf.projeto.models.Projeto;
+
+public interface ProjetoRepository extends JpaRepository<Projeto, Long>{
+
+}
